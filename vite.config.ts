19703a231ef3163c7f4230552fe2f6cc1b13cd24
Naufal Eko <naufaleko@example.com>
2026-07-15 22:22:25 +0700

    Restore Firebase Hosting GitHub Actions workflows

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/VirAshelle-Landing-Page/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
